3e2116c878f5edde35a373178825565abdc71967
gparquet <parquet.geoffrey@gmail.com>
2018-09-30 17:19:36 +0200

    feat: add get part for contact.

---
+++ contact/contact.service.js
@@ -0,0 +1,71 @@
+const _ = require('lodash');
+const uuidv4 = require('uuid/v4');
+
+const contacts = require('./contact.json');
+
+const commonFind = (allContact) => (propertyName, propertyValue) => {
+    
+     const userFounded = _.filter(allContact, (currentContact) => {
+        return currentContact[propertyName] === propertyValue;
+    });
+
+    if(userFounded) {
+        return userFounded;
+    }
+    return `User ${firstName} ${lastName} not found`;
+};
+
+
+const allContact = ()=>{
+    return contacts;
+};
+
+const findContactById = (allContacts = contacts.data) => (id) => {
+    return commonFind(allContacts)('id', id);
+};
+
+const findContactsByFirstAndLastName = (allContacts = contacts.data) => (firstName, lastName) => {
+    const userFounded = _.find(allContacts, (currentContact) => {
+        return currentContact.firstName === firstName && currentContact.lastName === lastName;
+    });
+
+    if(userFounded) {
+        return userFounded;
+    }
+    return `User ${firstName} ${lastName} not found`;
+};
+
+const findContactsByLastName = (allContacts = contacts.data) => (lastName) => {
+    return commonFind(allContacts)('lastName', lastName);
+};
+
+const addNewContact = (allContacts = contacts.data) => (contact) => {
+    let allContactTemp = allContacts;
+    const existedContact = findContactsByFirstAndLastName(allContactTemp)
+        (contact.firstName, contact.lastName);
+
+    if (!existedContact) {
+        allContacts.push({
+            id: uuidv4(),
+            firstName: contact.firstName,
+            lastName: contact.lastName,
+            phoneNumber: contact.phoneNumber,
+            email: contact.email
+        });
+        return allContactTemp;
+    }
+
+    return {
+        errorMessage: `This Contact with firstName : ${contact.firstName} and lastName : ${contact.lastName}`
+    }
+};
+
+const updateContact = () => {
+    
+};
+
+module.exports = {
+    findContactById: findContactById(),
+    allContact: allContact,
+    findContactsByFirstAndLastName: findContactsByFirstAndLastName()
+}
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 const bodyParser = require('body-parser');
 const fs = require('file-system');
 const _ = require('lodash');
-
+const contactService = require('./contact/contact.service');
 
 const port = 3000;
 const app = express();
@@ -19,17 +19,30 @@ if (mockUrl) {
         secure: false,
         ws: true,
         xfwd: true
-        })
+    })
     );
-}else{
+} else {
     app.use('/api', apiRoutes);
-    apiRoutes.route('/test').get((req, res) =>{
+    apiRoutes.route('/test').get((req, res) => {
         res.send('Mocks are running');
+    });
+    apiRoutes.route('/contacts').get((req, res) => {
+        setTimeout(() => {
+            res.send(contactService.allContact());
+        }, 200);
+    });
+    apiRoutes.route('/contacts/:id').get((req, res) => {
+        setTimeout(() => {
+            res.send(contactService.findContactById(req.params.id));
+        }, 200);
+    });
+    apiRoutes.route('/contacts/:firstName/:lastName').get((req, res) => {
+        setTimeout(() => {
+            res.send(contactService.findContactsByFirstAndLastName(req.params.firstName, req.params.lastName));
+        }, 200);
     });
 }
 
-
-
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Mock server listning on port : ${port}`)
 });
